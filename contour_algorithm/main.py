a2dff877351cbadb77af0ba9fbc8ac73da93c996
shlomo <shlomolo2@taxes.gov.il>
2025-11-12 16:00:55 +0200

    add alpha shape

--- contour_algorithm/main.py
+++ contour_algorithm/main.py
@@ -1,31 +1,14 @@
 import create_files_shoes
 import contour
-<<<<<<< HEAD:main.py
-import convex_hull
 import active_contour_snake
-import alphas_shape
-=======
->>>>>>> end24:contour_algorithm/main.py
 import distance_extremities
-#import contact_with_locations_TO_DELETE
 
 if __name__ == '__main__':
-<<<<<<< HEAD:main.py
-    # create_files_shoes.main()
-    # contour.main()
-    # convex_hull.main()
-    # active_contour_snake.main()
-    # alphas_shape.main()
-    # distance_extremities.main()
-    contact_with_locations.main()
-
-=======
     create_files_shoes.main()
     contour.main()
-    #active_contour_snake.main()
-    #distance_extremities.main()
+    active_contour_snake.main()
+    distance_extremities.main()
     
->>>>>>> end24:contour_algorithm/main.py
 
 
 
